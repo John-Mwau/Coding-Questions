2230b7573a770e2b886cceb968b09228d8b6e7f5
John-Mwau <jmulimwau@gmail.com>
2023-08-24 13:31:52 +0300

    first commit

---
+++ task1.js
@@ -0,0 +1,109 @@
+// question one
+let name="John";
+console.log(name);
+
+let country="Kenya";
+console.log(country);
+
+let isMasculine= true||false;
+console.log(isMasculine);
+
+// question two
+let counter=10;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+let counter1=(counter*2);
+console.log(counter1);
+
+// question 3
+let numString="42";
+let numValue=(+numString);
+let result1=(numValue + 10);
+console.log(result1);
+
+// question 4
+let x ="5" +3;
+console.log(x);
+console.log(typeof x);
+
+// question 5
+let myString=["cars"];
+console.log(myString.length);
+
+// question 6
+let lastName="Kelvin";
+console.log(typeof lastName);
+
+let pWeight=20;
+console.log(typeof pWeight);
+
+let b=true;
+console.log(typeof b);
+
+let personDetails={
+    firstName: "Daniel",
+    lastName: "Kibwana",
+    age: 40,
+}
+console.log(typeof personDetails);
+
+let c="";
+console.log(typeof C);
+
+let d=null;
+console.log(d);
+
+// question 7
+let w=8,
+    l=5;
+let areaRectangle=w*l;    
+console.log(areaRectangle); 
+
+// question 8
+let f=37;
+let Celcious=(f-32)*5/9
+console.log(Celcious);
+
+// question 9
+let integer=3;
+let Test=2;
+let rem=integer%Test;
+if (rem==0) {prompt("even number")
+} else if (rem==1){prompt ('old number')};
+
+// question 10
+let g=5
+let h=6
+let result2=(g>h);
+console.log(result2);
+
+// question 11
+let age="+currentage";
+let currentYear="+current year";
+let yearOfBirth=(currentYear-age);
+console.log(yearOfBirth);
+
+// question 12
+let t = '+u';
+let p= ('+u') + 10;
+let z=(String.valueOf(p));
+console.log(z);
+console.log(p);
+
+// question 13
+let text1=firstName="John";
+let text2 =otherName="Mwau";
+let message="Good Morning!";
+let output= message.concat(text1, text2);
+console.log(output);
+
+// question 14
+let y=5*(3+2)/2;
+console.log(y);
+
+
+
+
